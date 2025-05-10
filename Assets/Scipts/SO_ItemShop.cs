4e206e461ff43fb62504e8c28f79a428bedbe364
Ivan PM <cdmx2125@amerike.edu.mx>
2025-05-09 21:40:29 -0600

    Player Separated Textures and Skins Imported

---
+++ Assets/Scipts/SO_ItemShop.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemShopObj", menuName = "Scriptable Objects/ShopItemSO")]
+public class SO_ItemShop : ScriptableObject
+{
+    public Transform prefab;
+
+}
