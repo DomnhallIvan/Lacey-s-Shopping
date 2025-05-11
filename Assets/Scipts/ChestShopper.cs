7e7f73215d7d6ba685ff7c60679522f69e89d7d8
Ivan PM <cdmx2125@amerike.edu.mx>
2025-05-11 11:21:36 -0600

    WebGL Game

--- Assets/Scipts/ChestShopper.cs
+++ Assets/Scipts/ChestShopper.cs
@@ -6,6 +6,11 @@ public class ChestShopper : InteractionBehavior
 {
     [SerializeField] private Canvas _shopCanvas;
 
+    private void Start()
+    {
+        GameManager.instance.playerRef.ChangeSpeed(0);
+    }
+
     //WhenPlayerCloses the ShopMenu it should restart PlayerController speed from the Buttons.
     public override void Interact(PlayerInteraction player)
     {
